46f10ceafbccea1274b082b6aa6994ccda00dfe3
Javier Fonseca V <javier.fonseca@koombea.com>
2013-01-29 07:38:38 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wavetest::Application.config.secret_token = 'e6e2d75a647d0e783a2b1477e41aa337acbcfadab67503919547f8ead90be3b369a0fcf8885dd9e9a6352a6f1fc62420ced20a44c441b3beadb44d629cf6d3fd'
